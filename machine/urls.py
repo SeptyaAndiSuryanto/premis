b8dd10b3f09d6a08c00f0b453805d2daaa100277
Septya Andi Suryanto <septyaandi24@gmail.com>
2023-01-01 22:18:30 +0700

    category

--- machine/urls.py
+++ machine/urls.py
@@ -1,8 +1,17 @@
-from django.urls import path
-from .views import MachineIndex, MachineCategoryData
+from django.urls import path, re_path, include
+from .views import Category, MachineCategoryData
+
+
+category_urls = [
+    # re_path(r'(?P<pk>\d+)/', views.CategoryDetail.as_view(), name='category-detail')
+]
 
 urlpatterns = [
-    path("", MachineIndex.as_view(), name="machine-index"),
+    # re_path(r'^.*$', MachineIndex.as_view(), name='machine-index'),
+    path("category/", Category.as_view(), name="machine-category"),
     path('data/', MachineCategoryData.as_view()), 
+    re_path(r'^category/', include(category_urls)),
+
 
 ]
+
