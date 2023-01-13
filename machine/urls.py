a440a051b54142acd7fc27f4135545c45d1faf69
SeptyaAndiSuryanto <septyaandi24@gmail.com>
2023-01-13 07:12:22 +0700

    .

--- machine/urls.py
+++ machine/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path, re_path, include
+from . import views
 from .views import Category, MachineCategoryData
 
 
 category_urls = [
     # re_path(r'(?P<pk>\d+)/', views.CategoryDetail.as_view(), name='category-detail')
+    re_path(r'(?P<pk>\d+)/', include([
+        # re_path(r'^delete/', views.CategoryDelete.as_view(), name='category-delete'),
+        # Anything else
+        re_path(r'^.*$', views.CategoryDetail.as_view(), name='category-detail'),
+    ]))
 ]
 
 urlpatterns = [
